b30243be1642236131590f2188d48b7356ccf768
Bruno Rocha <30984260+brunorochaa@users.noreply.github.com>
2018-10-27 20:00:43 -0300

    Remove some links from Lineage
    remove overlay- lineage

--- device.mk
+++ device.mk
@@ -22,7 +22,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
