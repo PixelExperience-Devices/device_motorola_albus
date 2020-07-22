18e10ce00984134e7a47995b1e25751fc0e1d704
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2020-07-22 17:30:54 -0400

    albus: Update sepolicy for MotoMods
    * Make policy less generic and more specific to what we are
      defining. Move ModService to mods_app and define the attribute
      in public policy so it can be properly used across system
      vendor split within private and vendor policies.
    
    * Address mods_camd denials observed in logs
    
    * Add seinfo for ModService app
    
    * Allow ModService to read ro.boot.secure_hardware property
    
    Change-Id: I1bffe7e7b1ceb54bd91ce3f8b982cf94d9afe2ab
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,8 +56,9 @@ PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # SELinux
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-mods/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy-mods/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy-mods/public
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-mods/vendor
 
 # inherit from the proprietary version
 -include vendor/motorola/albus/BoardConfigVendor.mk
