c921baec0697f8aac919727c6604a745d732d020
jeangraff30 <jeangraff30@gmail.com>
2021-09-18 19:39:46 -0300

    albus: import idc and keylayout

--- device.mk
+++ device.mk
@@ -54,6 +54,11 @@ PRODUCT_PACKAGES += \
     CameraGo \
     libbson.vendor
 
+# Fingerprint Gestures
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc
+
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.sys.albus.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.sys.albus.rc
